22d58e63d462c26733dc472f2a00af8e8b8b0b02
HanaDzin <dzinhana@hotmail.com>
2024-11-02 15:49:12 +0100

    enabled cross-component communication with outputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,38 @@ import { UserInputComponent } from './user-input/user-input.component';
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent]
+  imports: [HeaderComponent, UserInputComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  calculateInvestmentResults(data: {
+    initialInvestment: number;
+    duration: number;
+    expectedReturn: number;
+    annualInvestment: number;
+  }) {
+    //destructure the incoming data into vars used in calculations
+    const { initialInvestment, duration, expectedReturn, annualInvestment } =
+      data;
+
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    console.log(annualData);
+  }
+}
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
   enteredInitialInvestment = '0';
@@ -14,12 +14,22 @@ export class UserInputComponent {
   enteredExpectedReturn = '5';
   enteredDuration = '10';
 
+  //property to be turned into a custom event (when clicked on button, calculations from app component execute)
+  @Output() calculate = new EventEmitter<{
+    initialInvestment: number;
+    duration: number;
+    expectedReturn: number;
+    annualInvestment: number;
+  }>();
 
   onSubmit() {
-    console.log(this.enteredInitialInvestment)
-    console.log(this.enteredAnnualInvestment)
-    console.log(this.enteredExpectedReturn)
-    console.log(this.enteredDuration)
+    //call the output property to emit the event that needs to be caught in app component
+    //taking into consideration that strings need to be converted to numbers - using the + symbol
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      duration: +this.enteredDuration,
+      expectedReturn: +this.enteredExpectedReturn,
+      annualInvestment: +this.enteredAnnualInvestment,
+    });
   }
-
 }
